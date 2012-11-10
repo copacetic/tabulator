2645b6f623f3109beb667649c9c30f63182de83a
Zachary Bush <zach@zmbush.com>
2012-11-09 20:26:19 -0800

    2 -> 3

--- upc_lookup.py
+++ upc_lookup.py
@@ -3,15 +3,15 @@
 #
 
 import sys
-from xmlrpclib import ServerProxy, Error
+from xmlrpc.client import ServerProxy, Error
 
 rpc_key = 'dd32f3999b03b5b3db78b3a75c7e3f91f256d9bb'
 
 if __name__=='__main__':
 	if len(sys.argv) != 2:
-		print 'Usage: fetchupc.py <upc>'
+		print('Usage: fetchupc.py <upc>')
 		exit
 	else:
 		s = ServerProxy('http://www.upcdatabase.com/xmlrpc')
 		params = { 'rpc_key': rpc_key, 'upc': sys.argv[1] }
-		print s.lookup(params)
+		print(s.lookup(params))
