285507e695cfc1e732ca89fecbf06f4bc8d78db3
Zachary Bush <zach@zmbush.com>
2012-11-10 08:10:45 -0800

    Add curses. Make barcode work with that

--- barcode.py
+++ barcode.py
@@ -3,9 +3,9 @@ SUFFIX='**'
 PREFIX_LENGTH=len(PREFIX)
 SUFFIX_LENGTH=len(SUFFIX)
 
-def scan():
+def scan(mainwin):
     while True:
-        code = input()
+        code = str(mainwin.getstr(), 'utf-8')
         if code[0:PREFIX_LENGTH] == PREFIX and code[-1*SUFFIX_LENGTH:] == SUFFIX:
             code = code[PREFIX_LENGTH:-SUFFIX_LENGTH]
             break
--- tabulator.py
+++ tabulator.py
@@ -6,8 +6,13 @@ import os
 import time
 import threading
 
-IDLE, PRIMED, USED = range(3)
+import curses
+
+IDLE, PRIMED, USED, PRICE_CHECK = range(4)
+stateStr = [ "IDLE", "PRIMED", "USED", "PRICE_CHECK" ]
 RFID, BARCODE = range(2)
+
+undo_card = 1253887727
 
 
 def RFIDThread(s):
@@ -20,22 +25,37 @@ def RFIDThread(s):
       elif s.state == PRIMED:
         if rfid == s.ID and s.Type == RFID:
           s.reset()
+        elif rfid == undo_card:
+          s.reset()
         else:
           s.startTransaction(rfid, RFID)
       elif s.state == USED:
         if rfid == s.ID and s.Type == RFID:
           s.checkout()
+        elif rfid == undo_card:
+          s.undo()
         else:
           s.startTransaction(rfid, RFID)
       TransactionState.lock.release()
       time.sleep(1)
 
-def BarcodeThread(s):
+def BarcodeThread(s, mainwin):
   while True:
-    code = barcode.scan()
+    code = barcode.scan(mainwin)
     TransactionState.lock.acquire()
     if s.state == IDLE:
-      s.startTransaction(code, BARCODE)
+      #record = db.get_record(1, code)
+      record = None
+      if int(code) == 23102492:
+        record = (BARCODE, 'Zachary Bush')
+      if record != None:
+        s.startTransaction(code, BARCODE)
+      else: 
+        s.priceCheck(upc.lookup(code))
+        TransactionState.lock.release()
+        time.sleep(3)
+        TransactionState.lock.acquire()
+        s.reset()
     elif s.state == PRIMED:
       if code == s.ID and s.Type == BARCODE:
         s.reset()
@@ -75,20 +95,73 @@ class TransactionState:
   def checkout(self):
     self.reset()
 
+  def undo(self):
+    self.stack.pop()
+    if len(self.stack) == 0:
+      self.state = PRIMED
+
+  def priceCheck(self, data):
+    self.state = PRICE_CHECK
+    self.pc_data = data
+
   def __str__(self):
-    retval = str(self.state) + "\n"
-    for t in self.stack:
-      retval += str(t) + "\n"
+    retval = stateStr[self.state]
+    if self.ID != None:
+      retval += " : "
+      retval += str(self.ID)
+      retval += " : %d transactions" % len(self.stack)
     return retval
 
+csua_rows = [
+' .d8888b.   .d8888b.  888     888       d8888',
+'d88P  Y88b d88P  Y88b 888     888      d88888',
+'888    888 Y88b.      888     888     d88P888',
+'888         "Y888b.   888     888    d88P 888',
+'888            "Y88b. 888     888   d88P  888',
+'888    888       "888 888     888  d88P   888',
+'Y88b  d88P Y88b  d88P Y88b. .d88P d8888888888',
+' "Y8888P"   "Y8888P"   "Y88888P" d88P     888'
+]
 
+price_check_rows = [
+' ######   ######     ###    ##    ##',
+'##    ## ##    ##   ## ##   ###   ##',
+'##       ##        ##   ##  ####  ##',   
+' ######  ##       ##     ## ## ## ##',
+'      ## ##       ######### ##  ####',
+'##    ## ##    ## ##     ## ##   ###',
+' ######   ######  ##     ## ##    ##',
+'',
+'#### ######## ######## ##       ##',
+' ##     ##    ##       ###     ###',
+' ##     ##    ##       ####   ####',
+' ##     ##    ######   ## ## ## ##',
+' ##     ##    ##       ##  ###  ##',
+' ##     ##    ##       ##       ##',
+'####    ##    ######## ##       ##'
+]
 
+login_rows = [
+' ######   ######     ###    ##    ##',
+'##    ## ##    ##   ## ##   ###   ##',
+'##       ##        ##   ##  ####  ##',   
+' ######  ##       ##     ## ## ## ##',
+'      ## ##       ######### ##  ####',
+'##    ## ##    ## ##     ## ##   ###',
+' ######   ######  ##     ## ##    ##',
+'',
+'#### #####   ',
+' ##  ##  ### ',
+' ##  ##    ##',
+' ##  ##    ##',
+' ##  ##    ##',
+' ##  ##  ### ',
+'#### #####   '
+]
 
-if __name__ == "__main__":
-  state = TransactionState()
-
+def graphics(stdscr, state):
   r = threading.Thread(target=RFIDThread, args=[state])
-  b = threading.Thread(target=BarcodeThread, args=[state])
+  b = threading.Thread(target=BarcodeThread, args=[state, stdscr])
 
   r.daemon = True
   b.daemon = True
@@ -96,7 +169,64 @@ if __name__ == "__main__":
   r.start()
   b.start()
 
+  height, width = stdscr.getmaxyx()
+
+  topWin = curses.newwin(10, 0)
+  leftWin = curses.newwin(height - 11, int(width/2), 10, 0)
+  rightWin = curses.newwin(height - 11, int(width/2), 10, int(width/2))
+  mainWin = curses.newwin(height - 11, 0, 10, 0)
+  statusBar = curses.newwin(1, 0, height - 1, 0)
+
+  topWin.border()
+  startrow = 1
+  for row in csua_rows:
+    startix = int(width / 2 - (len(row) / 2))
+    topWin.addstr(startrow, startix, row)
+    startrow += 1
+  topWin.refresh()
+
   while r.isAlive() and b.isAlive():
-    os.system('clear')
-    print(state)
-    time.sleep(1)
+    TransactionState.lock.acquire()
+    statusBar.erase()
+    statusBar.addstr(0, 0, str(state))
+    statusBar.refresh()
+    if state.state == IDLE:
+      leftWin.erase()
+      leftWin.border()
+
+      startrow = 1
+      for row in price_check_rows:
+        startix = int(width / 4 - (len(row) / 2))
+        leftWin.addstr(startrow, startix, row)
+        startrow += 1
+      t = "For a price check"
+      leftWin.addstr(startrow + 1, int(width / 4 - (len(t) / 2)), t)
+      leftWin.refresh()
+
+      rightWin.erase()
+      rightWin.border()
+
+      startrow = 1
+      for row in login_rows:
+        startix = int(width / 4 - (len(row) / 2))
+        rightWin.addstr(startrow, startix, row)
+        startrow += 1
+      t = "To begin a transaction"
+      rightWin.addstr(startrow + 1, int(width / 4 - (len(t) / 2)), t)
+      rightWin.refresh()
+      rightWin.refresh()
+    elif state.state == PRICE_CHECK:
+      mainWin.border()
+
+      mainWin.addstr(1, 1, str(state.pc_data))
+
+      mainWin.refresh()
+    else:
+      mainWin.border()
+      mainWin.refresh()
+    TransactionState.lock.release()
+    time.sleep(.5)
+
+if __name__ == "__main__":
+  state = TransactionState()
+  curses.wrapper(graphics, state)
