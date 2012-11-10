5d0047242d91bbc4dc1d6c1b450218db483d7e15
Hayg <haygastourian@berkeley.edu>
2012-11-09 20:38:41 -0800

    made upc lookup a function

--- upc_lookup.py
+++ upc_lookup.py
@@ -7,11 +7,15 @@ from xmlrpc.client import ServerProxy, Error
 
 rpc_key = 'dd32f3999b03b5b3db78b3a75c7e3f91f256d9bb'
 
+def upc_lookup(upc):
+    s = ServerProxy('http://www.upcdatabase.com/xmlrpc')
+    params = { 'rpc_key': rpc_key, 'upc': upc }
+    return s.lookup(params)
+
 if __name__=='__main__':
 	if len(sys.argv) != 2:
-		print('Usage: fetchupc.py <upc>')
-		exit
-	else:
-		s = ServerProxy('http://www.upcdatabase.com/xmlrpc')
-		params = { 'rpc_key': rpc_key, 'upc': sys.argv[1] }
-		print(s.lookup(params))
+            print('Usage: fetchupc.py <upc>')
+            exit
+        else:
+            print(upc_lookup(sys.argv[1]))
+             
