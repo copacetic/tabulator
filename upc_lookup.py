5d101f78542f4da6e0a61421ecfa35ca58628585
Hayg <haygastourian@berkeley.edu>
2012-11-09 20:19:56 -0800

    Added upc_lookup.py which has the code to lookup against upcdatabase.com

---
+++ upc_lookup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#
+#
+
+import sys
+from xmlrpclib import ServerProxy, Error
+
+rpc_key = 'dd32f3999b03b5b3db78b3a75c7e3f91f256d9bb'
+
+if __name__=='__main__':
+	if len(sys.argv) != 2:
+		print 'Usage: fetchupc.py <upc>'
+		exit
+	else:
+		s = ServerProxy('http://www.upcdatabase.com/xmlrpc')
+		params = { 'rpc_key': rpc_key, 'upc': sys.argv[1] }
+		print s.lookup(params)
